e3a70d4ba6ef5ee17024085c7405a924205a6c1e
VMNhatJr10 <74690179+nhat123-dev@users.noreply.github.com>
2021-09-20 15:56:10 +0700

    Update StudentManagement.java

--- src/StudentManagement.java
+++ src/StudentManagement.java
@@ -1,47 +1,77 @@
-import java.util.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class StudentManagement {
-    private final ArrayList<Student> students = new ArrayList<>();
+    private final Student[] students = new Student[100];
 
+    /**
+     * sameGroup is checked relationship of group.
+     * @param s1 is group of student 1.
+     * @param s2 is group of student 2.
+     * @return
+     */
     public static boolean sameGroup(Student s1, Student s2) {
         return s1.getGroup().equals(s2.getGroup());
     }
 
+    /**
+     * addStudent.
+     * @param newStudent is new student.
+     */
     public void addStudent(Student newStudent) {
-        students.add(newStudent);
+        for (int i = 0; i < students.length; i++) {
+            if (students[i] == null) {
+                students[i] = newStudent;
+                break;
+            }
+        }
     }
 
+    /**
+     * get students by group.
+     * @return students by group.
+     */
     public String studentsByGroup() {
-        StringBuffer sBG = new StringBuffer();
-        Set<String> sortGroup = new LinkedHashSet<>();
-        for (int i = 0; i < students.size(); i++) {
-            sortGroup.add(students.get(i).getGroup());
+        StringBuffer res = new StringBuffer();
+        Set<String> gr = new LinkedHashSet<>();
+        for (int i = 0; i < students.length; i++) {
+            if (students[i] == null) {
+                break;
+            } else {
+                gr.add(students[i].getGroup());
+            }
         }
-        for (String elements : sortGroup) {
-            sBG.append(elements);
-            sBG.append("\n");
-            for (int i = 0; i < students.size(); i++) {
-                if (students.get(i).getGroup().equals(elements)) {
-                    sBG.append(students.get(i).getInfo());
-                    sBG.append("\n");
+
+        for (String element : gr) {
+            res.append(element).append("\n");
+            for (int i = 0; i < students.length; i++) {
+                if (students[i] != null) {
+                    if (students[i].getGroup().equals(element)) {
+                        res.append(students[i].getInfo()).append("\n");
+                    }
+                } else {
+                    continue;
                 }
             }
         }
-        return sBG.toString();
+        return res.toString();
     }
 
+    /**
+     * remove student.
+     * @param id is an id.
+     */
     public void removeStudent(String id) {
-        for (int i = 0; i < students.size(); i++) {
-            if (students.get(i).getID().equals(id)) {
-                students.remove(students.get(i));
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].getId().equals(id)) {
+                for (; i < students.length - 1; i++) {
+                    students[i] = students[i + 1];
+                }
             }
         }
     }
 
     public static void main(String[] args) {
 
-
     }
-
 }
-
