1cd3719e1ec9d1860fed083d117a4e0a5bc06e14
VMNhatJr10 <74690179+nhat123-dev@users.noreply.github.com>
2021-09-20 16:02:22 +0700

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,69 +1,63 @@
-
-
 public class Student {
     private String name;
     private String id;
     private String group;
     private String email;
 
-    // constructor
-    public Student() {
+    Student() {
         this.name = "Student";
         this.id = "000";
         this.group = "K62CB";
         this.email = "uet@vnu.edu.vn";
     }
 
-    public Student(String name, String id, String email) {
+    Student(String name, String id, String email) {
         this.name = name;
         this.id = id;
         this.group = "K62CB";
         this.email = email;
     }
 
-    public Student(Student s) {
+    Student(Student s) {
         this.name = s.name;
         this.id = s.id;
         this.group = s.group;
         this.email = s.email;
     }
-    // set, getter
+
     public String getName() {
         return name;
     }
 
-    public void setName(String Name) {
-        this.name = Name;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getGroup() {
         return group;
     }
 
-    public void setGroup(String Group) {
-        this.group = Group;
-    }
-
-    public String getID() {
-        return id;
-    }
-
-    public void setId(String ID) {
-        this.id = ID;
+    public void setGroup(String group) {
+        this.group = group;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String Email) {
-        this.email = Email;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    // get information of student
     public String getInfo() {
         return name + " - " + id + " - " + group + " - " + email;
     }
-
 }
-
