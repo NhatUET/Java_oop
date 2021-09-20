95a345cfee36909eeba5884a65d784feb0f530f8
VMNhatJr10 <74690179+nhat123-dev@users.noreply.github.com>
2021-09-20 11:02:43 +0700

    Update StudentManagement.java

--- src/StudentManagement.java
+++ src/StudentManagement.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
 public class StudentManagement {
-    private ArrayList<Student> students = new ArrayList<>();
+    private final ArrayList<Student> students = new ArrayList<>();
 
-    public static boolean sameGroup(Student s1, Student s2){
-        return s1.group == s2.group;
+    public static boolean sameGroup(Student s1, Student s2) {
+        return s1.getGroup().equals(s2.getGroup());
     }
 
     public void addStudent(Student newStudent) {
@@ -12,17 +12,18 @@ public class StudentManagement {
     }
 
     public String studentsByGroup() {
-        ArrayList<String> sBG = new ArrayList<>();
-        Set<String> sortGroup = new HashSet<>();
-        for(int i = 0; i < students.size(); i++) {
-            sortGroup.add(students.get(i).group);
+        StringBuffer sBG = new StringBuffer();
+        Set<String> sortGroup = new LinkedHashSet<>();
+        for (int i = 0; i < students.size(); i++) {
+            sortGroup.add(students.get(i).getGroup());
         }
-        //Iterator<String> it = sortGroup.iterator();
-        for(String element:sortGroup) {
-            sBG.add(element.toString());
+        for (String elements : sortGroup) {
+            sBG.append(elements);
+            sBG.append("\n");
             for (int i = 0; i < students.size(); i++) {
-                if (students.get(i).group == element.toString()) {
-                    sBG.add(students.get(i).getInfor());
+                if (students.get(i).getGroup().equals(elements)) {
+                    sBG.append(students.get(i).getInfo());
+                    sBG.append("\n");
                 }
             }
         }
@@ -30,51 +31,17 @@ public class StudentManagement {
     }
 
     public void removeStudent(String id) {
-        int pos = 0;
-        for(int i = 0; i < students.size(); i++) {
-            if(students.get(i).id == id) {
-                pos = i;
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).getID().equals(id)) {
+                students.remove(students.get(i));
             }
         }
-        students.remove(students.get(pos));
     }
-    
+
     public static void main(String[] args) {
-        // make object
-        Student sv1 = new Student("vu minh nhat" , "20021407", "K65CACLC3","20021407@vnu.edu.vn");
-        Student sv2 = new Student("tran van duy", "20021319", "K65CACLC2","20021319@vnu.edu.vn");
-        Student sv3 = new Student("dao van quyen", "20021422", "K65CACLC2","20021422@vnu.edu.vn");
-        Student sv4 = new Student("le anh tuan", "20021234", "K65NCLC1","21231@vnu.edu.vn");
-
-        //System.out.println(sv1.getInfor());
-        sv1.setName("Vu Minh Nhat"); // set name
-        System.out.println(sv1.getInfor()); // get information of obj
-        System.out.println(sv1.getName()); // get name of obj
-
-        Student sv5 = new Student(sv1);
-        System.out.println(sv5.getInfor());
 
 
-        // make a array object
-        StudentManagement lophoc = new StudentManagement();
-        // add student
-        lophoc.addStudent(sv1);
-        lophoc.addStudent(sv2);
-        lophoc.addStudent(sv3);
-        lophoc.addStudent(sv4);
-
-        //lophoc.studentsByGroup();
-        // print the list of student
-        String gr = lophoc.studentsByGroup();
-        for(int i = 0; i < gr.length(); i++){
-            System.out.print(gr.charAt(i));
-        }
-        lophoc.removeStudent("20021319");
-
-        String gr1 = lophoc.studentsByGroup();
-        for(int i = 0; i < gr1.length(); i++){
-            System.out.print(gr1.charAt(i));
-        }
-        //System.out.println(lophoc.sameGroup(sv1,sv2));
     }
+
 }
+
